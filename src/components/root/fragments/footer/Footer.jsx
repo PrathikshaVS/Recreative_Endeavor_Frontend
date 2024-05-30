aa9928a76459563c3b5f1e2f04e9495fb8f55e1a
Prathiksha VS <prathiksha4915@gmail.com>
2024-05-30 15:09:24 +0530

    Recreative endeavor habbies First commit

---
+++ src/components/root/fragments/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Footer = (props) => {
+  return (
+    <footer className={props.class}>
+      &copy; Recreative Endeavor 2023. Designed and Developed by Prathiksha VS.
+    </footer>
+  );
+};
+
+export default Footer;
